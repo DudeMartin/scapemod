8bc16cf9ac198e1db83c70342dfb4855c4289816
Martin Tuskevicius <mtuspersonal@hotmail.com>
2014-12-23 14:56:02 -0600

    Tiny accessor (aesthetic) modifications for consistency.

--- src/org/scapemod/accessor/Region.java
+++ src/org/scapemod/accessor/Region.java
@@ -1,9 +1,7 @@
 package org.scapemod.accessor;
 
-/**
- * Created by luckruns0ut on 23/12/2014.
- */
 public interface Region {
+
     Tile[][][] getTiles();
 
     InteractableObject[] getObjects();

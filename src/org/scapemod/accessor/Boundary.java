a5abb44ed415054ecaa22aa15883154828958583
Jordan <jordanwhittle1@gmail.com>
2014-12-24 18:05:59 +0000

    Added a variety of new hooks.

---
+++ src/org/scapemod/accessor/Boundary.java
@@ -0,0 +1,17 @@
+package org.scapemod.accessor;
+
+/**
+ * Created by luckruns0ut on 24/12/2014.
+ */
+public interface Boundary {
+
+    RenderableNode getRenderableNode();
+
+    int getHash();
+
+    int getPlane();
+
+    int getX();
+
+    int getY();
+}
